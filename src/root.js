7f11bfb2fe4a1b5cefb448d93f2c264d9aa7bbf6
Eric <ericnavar@ufl.edu>
2020-01-25 15:35:16 -0500

    add root.js, Tabs.js, Linkedin link.txt -add root.js from AskAGator, but with all the code commented out since my code doesn't have the correct dependencies -Linkedin link.txt has the HTML for a component that would link to my LinkedIn profile, provided by LinkedIn -Tabs.js has code copied from a Material UI component file

---
+++ src/root.js
@@ -0,0 +1,56 @@
+/*
+import * as React from 'react'
+import { BrowserRouter, Route, Switch, withRouter } from 'react-router-dom'
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+import { AUTH_TOKEN } from './constants';
+
+import HomePage from './pages/index';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import AddCoursePage from './pages/AddCoursePage';
+import ProfilePage from './pages/ProfilePage';
+import EditProfilePage from './pages/EditProfilePage';
+import Sandbox from './pages/SandboxPage';
+import Header from './components/Header';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/api',
+  fetchOptions: {
+    credentials: 'include'
+  },
+  request: async (operation) => {
+    const token = await localStorage.getItem(AUTH_TOKEN);
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      }
+    });
+  },
+  onError: ({ graphQLErrors, networkError }) => {
+    // Do something with the errors lol
+  }
+});
+
+export default class Root extends React.Component {
+  public render(): JSX.Element {
+    return (
+      <BrowserRouter>
+        <ApolloProvider client={client}>
+        <Header/>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/login" component={withRouter(LoginPage)} />
+
+            <Route path="/signup" component={withRouter(SignUpPage)} />
+            <Route path="/addcourse" component={withRouter(AddCoursePage)} />
+            <Route path="/profile" component={withRouter(ProfilePage)} />
+            <Route path="/editprofile" component={withRouter(EditProfilePage)} />
+          </Switch>
+          </ApolloProvider>
+      </BrowserRouter>
+    )
+  }
+}
+*/

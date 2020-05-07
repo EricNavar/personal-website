3b9e816410f7233d4ebd8180fbe80e0c8c45beb4
EricNavar <ericnavar@ufl.edu>
2020-05-07 14:33:56 -0400

    restyle and refactor Skill List move the SkillItem component to SkillList.js and delete SkillItem.js make the Skill List a table change the color of the rating coomponent move the image before the text add spacing between the image and text

--- src/components/SkillList.js
+++ src/components/SkillList.js
@@ -1,19 +1,67 @@
 import React from 'react';
-import SkillItem from './SkillItem';
 import Section from './Section';
 import JavaIcon from './../assets/images/java.png';
 import ReactIcon from './../assets/images/React.png';
 import CppIcon from './../assets/images/Cpp.png';
 import GitIcon from './../assets/images/Git.png';
+import Typography from '@material-ui/core/Typography';
+import Rating from '@material-ui/lab/Rating';
+import { makeStyles } from '@material-ui/core/styles';
+import LensIcon from '@material-ui/icons/Lens';
 
 //icons must be PNGs
+const useStyles = makeStyles({
+  table: {
+    '& td': {
+      paddingRight: 4,
+      paddingLeft: 4,
+      paddingTop:8,
+      paddingBottom: 8
+    }
+  },
+  img: {
+    width:32,
+    borderRadius: 8
+  }
+});
+
+function SkillItem({skill, icon, value}) {
+  const classes = useStyles();
+  return (
+    <tr>
+      <td>
+        <img alt="" className={classes.img} src={icon}/>
+      </td>
+      <td>
+        <Typography>
+          {skill}
+        </Typography>
+      </td>
+      <td>
+        <Rating
+          name="read-only"
+          value={value}
+          readOnly
+          icon={<LensIcon/>}
+          style={{color:'#46b5d1'}}
+        />
+      </td>
+    </tr>
+  );
+}
+
 export default function SkillList() {
+  const classes = useStyles();
   return(
     <Section title = "Skills">
-      <SkillItem skill="C++" value={4} icon={CppIcon}/>
-      <SkillItem skill="Java" value={3} icon={JavaIcon}/>
-      <SkillItem skill="React" value={2} icon={ReactIcon}/>
-      <SkillItem skill="Git" value={3} icon={GitIcon}/>
+      <table className={classes.table}>
+        <tbody>
+          <SkillItem skill="C++" value={4} icon={CppIcon}/>
+          <SkillItem skill="Java" value={3} icon={JavaIcon}/>
+          <SkillItem skill="React" value={2} icon={ReactIcon}/>
+          <SkillItem skill="Git" value={3} icon={GitIcon}/>
+        </tbody>
+      </table>
     </Section>
   );
 }

0d106c1cd3bc9c1dbff92d903b4524e9bf35e4ce
EricNavar <ericnavar@ufl.edu>
2020-10-10 20:12:27 -0400

    implement gato noches page

--- src/screens/GatoNoches.js
+++ src/screens/GatoNoches.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React from 'react';
+import Link from '@material-ui/core/Link';
+import GatoNochesLogo from './../assets/svg/GatoNochesLogo';
+import Typography from "@material-ui/core/Typography";
+import {makeStyles} from "@material-ui/core/styles";
+import {Link as RouteLink} from "react-router-dom";
+
+const useStyles = makeStyles({
+  gatoNoches: {
+    minHeight: "100vh",
+    width: "100%",
+    padding: 32,
+    backgroundColor: "#F9E9EA",
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  }
+});
 
 export default function GatoNoches() {
+  const classes = useStyles();
+  React.useEffect(() => {
+    document.title = 'Gato Noches';
+  });
   return (
-    <div></div>
+    <div className={classes.gatoNoches}>
+      <div style={{maxWidth:350}}>
+        <GatoNochesLogo/>
+      </div>
+      <Typography style={{marginTop: 12, marginBottom: 12}}>
+        Due to the Covid-19 pandemic, no events for Gato Noches are planned right now
+      </Typography>
+      <Link component={RouteLink} to='/'>
+        Return to home
+      </Link>
+    </div>
   );
 }

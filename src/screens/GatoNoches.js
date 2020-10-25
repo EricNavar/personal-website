c9faa47b6abede991b6276f003c996d663d52e0b
EricNavar <ericnavar@ufl.edu>
2020-10-25 15:13:52 -0400

    add cat paw cursor

--- src/screens/GatoNoches.js
+++ src/screens/GatoNoches.js
@@ -18,7 +18,8 @@ const useStyles = makeStyles({
     flexDirection: 'column',
     justifyContent: 'flex-start',
     alignItems: 'center',
-    textAlign: "center"
+    textAlign: "center",
+    cursor: "url(\"https://lh3.googleusercontent.com/pw/ACtC-3d-YhK0Cdb66QP67wA8ZzKDOpDV9m3sG88DdcwXq5ubNX6xePR7Ai0F7UMr1rHOw5Z9wRovbDJDdSi-4Bpzk7GA8EKZS2-rxsn1CBuVAOEcAHksdTjRE3LU20RujF9tKARurzRELIbtHlMjAOzd5H9o=w64-h57-no\"), auto"
   }
 });
 

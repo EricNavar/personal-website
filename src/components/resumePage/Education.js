8f3afe049a855bbba76d9c84f4c65526bd13a095
EricNavar <ericnavar@ufl.edu>
2021-01-24 16:19:56 -0500

    Restyle Education - show the course list with Chips instead of a List component - update GPA and add new courses - take out graduation date - insert major and minor

--- src/components/resumePage/Education.js
+++ src/components/resumePage/Education.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
 import Typography from '@material-ui/core/Typography';
+import Chip from '@material-ui/core/Chip';
 import Section from './Section.js';
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -11,6 +10,23 @@ const useStyles = makeStyles({
     '& b': {
       color: '#151965'
     }
+  },
+  courseworkItemsWrapper: {
+    display: 'flex',
+    justifyContent: 'left',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: 4
+    },
+    position: "relative"
+  },
+  courseworkItem: {
+    backgroundImage: "linear-gradient(#42b3f5, #9042f5)",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundAttachment: "fixed",
+    color: "white",
+    border: 0
   }
 });
 
@@ -23,37 +39,40 @@ export default function Education() {
     "Information and Database Systems",
     "Intro to Software Engineering",
     "Programming Language Concepts",
-    "Professional Communication for Engineers"
+    "Professional Communication for Engineers",
+    "Operating Systems",
+    "Project Methodologies",
+    "Principles of Interaction and Usability",
+    "Natural Language Processing"
   ];
-  function CourseworkListItems() {
+  function CourseworkItems() {
     return (
       coursework.map(course => 
-        <ListItem key={course} divider dense color='textPrimary' style={{paddingTop: 0}}>
-          <Typography variant='body1' color='textPrimary'>
-            {course}
-          </Typography>
-        </ListItem>
+        <Chip label={course} variant='body1' className={classes.courseworkItem}/>
       )
     )
   };
 
   return (
     <Section title="Education">
-      <Typography component='span' variant='h5' color='textPrimary'>
+      <Typography variant='h5' color='textPrimary'>
         University of Florida
       </Typography>
-      <Typography component='span' variant='overline' color='textSecondary' style={{marginLeft: 16, fontSize:14}}>
-        May 2022
+      <Typography className={classes.gpa} component="span" variant='body1' color='textPrimary'>
+        Computer Science Major
+      </Typography>
+      <Typography className={classes.gpa} component="span" variant='body1' color='textSecondary'>
+        &nbsp; | Digital Arts Minor
       </Typography>
       <Typography className={classes.gpa} variant='body1' color='textPrimary'>
-        <b>GPA:</b> 3.93
+        <b>GPA:</b> 3.95
       </Typography>
       <Typography component='p' variant='h6' color='textPrimary'>
         Relevant Coursework:
       </Typography>
-      <List style={{width:'max-content'}}>
-        <CourseworkListItems/>
-      </List>
+      <div className={classes.courseworkItemsWrapper}>
+        <CourseworkItems/>
+      </div>
     </Section>
   );
 }

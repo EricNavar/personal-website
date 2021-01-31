265c6c68a7b4c5cd2b44bd7e3750a5b0e8cfb8d3
EricNavar <ericnavar@ufl.edu>
2021-01-31 11:31:30 -0500

    remove unused import

--- src/components/worstWebsite/Footer.js
+++ src/components/worstWebsite/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { makeStyles, ButtonBase, Grid, IconButton /*,Typography*/ } from '@material-ui/core';
-import { Link } from 'react-router-dom';
 import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles(theme =>({

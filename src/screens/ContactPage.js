8fcb22524a3bd60e15712635da94149bb489e08a
EricNavar <ericnavar@ufl.edu>
2021-04-12 23:55:39 -0400

    increase opacity on hover and add inset box shadow on active for submit button

--- src/screens/ContactPage.js
+++ src/screens/ContactPage.js
@@ -32,10 +32,10 @@ const useStyles = makeStyles((theme) => ({
     padding: '0 30px',
     width: 200,
     '&:hover': {
-      opacity: .8
+      opacity: .85
     },
     '&:active': {
-      boxShadow: 'none'
+      boxShadow: 'rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset'
     }
   },
   link: {

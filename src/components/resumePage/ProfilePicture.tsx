eeacb9223042b3d164fe9da099f89aa2b0d5e869
EricNavar <ericnavar@ufl.edu>
2021-11-09 21:36:56 -0500

    rename ProfilePicture component function

--- src/components/resumePage/ProfilePicture.tsx
+++ src/components/resumePage/ProfilePicture.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Eric from './../../assets/other/Eric Navar.webp';
 
-function PersonalStatement() {
+function ProfilePicture() {
   return (
     <img width="100%" height="100%" src={Eric} alt="Eric profile" style={{ borderRadius: '50%' }} />
   );
 }
 
-export { PersonalStatement }
+export { ProfilePicture }
